5922803381999290f120ac58382cc302da7776dd
mao0000 <maopuregummy393@gmail.com>
2025-12-29 15:43:44 +0900

    単体テストコードの修正

--- spec/models/order_shipping_address_spec.rb
+++ spec/models/order_shipping_address_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe OrderShippingAddress, type: :model do
   before do
-    @order_shipping_address = FactoryBot.build(:order_shipping_address, user_id: 1, item_id: 1)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    sleep 0.1
+    @order_shipping_address = FactoryBot.build(:order_shipping_address, user_id: user.id, item_id: item.id)
   end
 
   describe '購入情報の保存' do
